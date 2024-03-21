fecf76f47bf7e890f36adb3f11d4cdf8ba598a77
Amy Au <aa4644@drexel.edu>
2024-03-21 08:26:21 -0400

    homepage

---
+++ javascript/general.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
